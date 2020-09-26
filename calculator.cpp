796420acf720adc6efd431e4ecc035b05e9b2da1
Jasmeen <jasbansal56@gmail.com>
2020-09-26 18:37:08 +0530

    Added minus and space functionality

--- calculator.cpp
+++ calculator.cpp
@@ -1,22 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string INTEGER = "INTEGER", PLUS = "PLUS", EOL = "EOL";
+string INTEGER = "INTEGER", PLUS = "PLUS", MINUS = "MINUS", EOL = "EOL";
 
 class Token
 {
     public:
 
-        int value; //will store the value in ASCII
+        string value; //will store the value in string
         string type; //will store the word
 
         Token()
         {
-            this->value = 0;
+            this->value = "";
             this->type = "";
         }
 
-        Token(string type, int value)
+        Token(string type, string value)
         {
             this->value = value;
             this->type = type;
@@ -25,7 +25,7 @@ class Token
 
 //To print the token in the correct format
 ostream& operator<<(ostream &strm, const Token &token) {
-    return strm << "(" << token.type << ", "<<char(token.value)<<")";
+    return strm << "(" << token.type << ", "<<token.value<<")";
 }
 
 class Interpreter
@@ -33,6 +33,7 @@ class Interpreter
     string text;
     int pos;
     Token current_token;
+    char current_char;
 
     void error()
     {
@@ -40,35 +41,79 @@ class Interpreter
         _Exit(10);
     }
 
+    void advance()
+    {
+        pos++;
+        cout<<current_char<<"\n";
+        if(pos > text.size()-1)
+            current_char = '\0';
+        else
+            current_char = text[pos];
+    }
+
+    void skip_whitspace()
+    {
+        while(current_char != '\0' && current_char == ' ')
+            advance();
+    }
+
+    string integer_val()
+    {
+        string result = "";
+        while(current_char != '\0' && current_char >= 48 && current_char <= 57)
+        {
+            result += current_char;
+            advance();
+        }
+        return result;
+    }
+
 /* ******LEXICAL ANALYSER****** */
     Token get_next_token()
     {
-        if(pos > text.size()-1)
-            return Token(EOL, '\0');
-
-        char current_char = text[pos];
-
-        //Means we have a digit
-        if(current_char >= 48 && current_char <= 57)
+        cout<<"CHAR" <<current_char<<"\n";
+        while(current_char != '\0')
         {
-            Token token(INTEGER, current_char);
-            pos++;
-            return token;
-        }
+            if(current_char == ' ')
+            {
+                skip_whitspace();
+                continue;
+            }
 
-        if(current_char == '+')
-        {
-            Token token(PLUS, current_char);
-            pos++;
-            return token;
-        }
+            string current_char_str;
+            current_char_str = current_char;
 
-        //If it isnt a digit or +, then some other char, hence show error
-        error();
+            //Means we have a digit
+            if(current_char >= 48 && current_char <= 57)
+            {
+                Token token(INTEGER, integer_val());
+                return token;
+            }
+
+            if(current_char == '+')
+            {
+                Token token(PLUS, current_char_str);
+                advance();
+                return token;
+            }
+
+            if(current_char == '-')
+            {
+                Token token(MINUS, current_char_str);
+                advance();
+                return token;
+            }
+
+            //If it isnt a digit or +, then some other char, hence show error
+            error();
+        }
+        return Token(EOL, "\0");
     }
 
     void eat(string type)
     {
+        // cout<<current_token.type<<"\n";
+        cout<<type<<"\n";
         if(current_token.type == type)
             current_token = get_next_token();
         else
@@ -80,21 +125,30 @@ class Interpreter
         {
             this->text = text;
             this->pos = 0;
+            this->current_char = text[pos];
         }
 
         int eval()
         {
             current_token = get_next_token();
 
-            int value1 = (current_token.value)-48;
+            int value1 = stoi(current_token.value);
             eat(INTEGER);
 
-            eat(PLUS);
+            string operator_type = current_token.type;
 
-            int value2 = (current_token.value)-48;
+            if(operator_type == "PLUS")
+                eat(PLUS);
+            else
+                eat(MINUS);
+
+            int value2 = stoi(current_token.value);
             eat(INTEGER);
 
-            return (value1 + value2);
+            if(operator_type == "PLUS")
+                return (value1 + value2);
+            else
+                return (value1 - value2);
         }
 };
 
