bd83a5abcf74d6ae9678ca303b5d86a66afc7881
unknown <bx0709@gmail.com>
2020-12-12 23:12:26 +0530

    Implemented Call Stack

--- interpreter.h
+++ interpreter.h
@@ -1,14 +1,80 @@
 #include "symbol.h"
+#include <stack>
 
 /* ###############################
    #       INTERPRETER           #
    ###############################
 */
 
+//enum used in original code
+class ARType
+{
+public:
+    string PROGRAM = "PROGRAM";
+    string PROCEDURE = "PROCEDURE";
+};
+
+class ActivationRecord 
+{
+public:
+    string name;
+    string type;
+    int nestingLevel;
+    unordered_map<string, float> members;
+
+    ActivationRecord(){}
+
+    ActivationRecord(string name, string type, int nestingLevel) 
+    {
+        this->name = name;
+        this->type = type;
+        this->nestingLevel = nestingLevel;
+    }
+
+    void setItem(string key, float value)
+    {
+        members[key] = value;
+    }
+
+    float getItem(string key)
+    {
+        return members[key];
+    }
+    //can add get function which does not throw exception if key not found
+    //can also add function for printing the activation record
+};
+
+class CallStack 
+{
+
+public:
+    stack<ActivationRecord> records;
+
+    //Name of function can be changed, to avoid recursion
+    void push(ActivationRecord ar)
+    {
+        this->records.push(ar);
+    }
+
+    ActivationRecord pop()
+    {
+        ActivationRecord element = records.top();
+        records.pop();
+        return element;
+    }
+
+    ActivationRecord peek()
+    {
+        return records.top();
+    } 
+    //can add function for printing the stack
+};
+
 
 class Interpreter
 {
     boostvar tree;
+    CallStack call_stack;
 
 public:
     Interpreter(boostvar tree)
@@ -107,8 +173,9 @@ public:
     int visit_Assign(Assign* node)
     {
         string var_name = boost::get<Var*>(node->left)->value;
-        GLOBAL_SCOPE[var_name] = visit(node->right);
-        return GLOBAL_SCOPE[var_name];
+        ActivationRecord ar = this->call_stack.peek();
+        ar.members[var_name] = visit(node->right);
+        return ar.members[var_name];
     }
 
     int visit_Print(Print* node)
@@ -122,12 +189,14 @@ public:
     int visit_Var(Var* node)
     {
         string var_name = node->value;
-        if (GLOBAL_SCOPE.find(var_name) == GLOBAL_SCOPE.end())
+        ActivationRecord ar = this->call_stack.peek();
+        //Need to make changes here, i guess... for the function to access non-local variables
+        if (ar.members.find(var_name) == ar.members.end())
         {
             cout << "ERROR:: Variable "<< var_name <<" not defined.\n";
             _Exit(10);
         }
-        return GLOBAL_SCOPE[var_name];
+        return ar.members[var_name];
     }
 
     int visit_Type(Type* node)
@@ -146,7 +215,7 @@ public:
 
     int visit_ProcedureCall(ProcedureCall* node)
     {
-        if (node->proc_symbol)
+        /*if (node->proc_symbol)
         {
             ProcedureSymbol* proc_symbol = node->proc_symbol;
 
@@ -168,8 +237,22 @@ public:
             }
 
             visit(proc_symbol->block_node);
+        }*/
+        string proc_name = node->proc_name;
+        ARType ar_type;
+        ActivationRecord ar(proc_name, ar_type.PROCEDURE, 2);
+        ProcedureSymbol* proc_symbol = node->proc_symbol;
+        vector<boostvar> formal_params = proc_symbol->params;
+        vector<boostvar> actual_params = node->actual_params;
+        for (int i = 0; i < formal_params.size(); i++)
+        {
+            VarSymbol* var = boost::get<VarSymbol*>(formal_params[i]);
+            string var_name = var->name;
+            ar.members[var_name] = visit(actual_params[i]);
         }
-        
+        this->call_stack.push(ar);
+        visit(proc_symbol->block_node);
+        this->call_stack.pop();
         return 0;
     }
 
@@ -184,7 +267,12 @@ public:
 
     int visit_Program(Program* node)
     {
+        string program_name = node->name;
+        ARType ar_object;
+        ActivationRecord ar(program_name, ar_object.PROGRAM, 1);
+        this->call_stack.push(ar);
         visit(node->block);
+        this->call_stack.pop();
         return 0;
     }
 
@@ -192,4 +280,5 @@ public:
     {
         return visit(tree);
     }
+    //Have not implemented the log function that prints the call stack
 };
